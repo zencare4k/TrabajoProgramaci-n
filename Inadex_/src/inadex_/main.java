781a1dbd6eb0162e1817e9b1bb869c639d16dcea
z4k23 <122319845+z4k23@users.noreply.github.com>
2024-05-01 12:52:58 +0200

    Project Update

---
+++ Inadex_/src/inadex_/main.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inadex_;
+
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Carlos Marban
+ */
+
+
+public class main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FontFormatException {
+
+
+    }
+    
+}
